e786e4444dc5e9211e87474163507966e2780823
altjohn143 <johnmatthewolano4@gmail.com>
2026-05-18 02:10:53 +0800

    asd

--- web-app/src/pages/PublicLotMap.js
+++ web-app/src/pages/PublicLotMap.js
@@ -66,15 +66,15 @@ const IMAGE_BLOCK_FRAMES = {
 // PHASE 1
 1: { left: 14.00, top: 19.40, width: 22.00, height: 2.45, rotate: -2.30 },
 
-// slightly left
-2: { left: 14.20, top: 26.25, width: 22.00, height: 2.45, rotate: -3.20 },
+// slightly more tilt left
+2: { left: 14.20, top: 26.25, width: 22.00, height: 2.45, rotate: -3.40 },
 
-3: { left: 14.40, top: 32.20, width: 22.00, height: 2.45, rotate: -2.90 },
+// slightly tilt left
+3: { left: 14.40, top: 32.20, width: 22.00, height: 2.45, rotate: -3.10 },
 
-// slightly move down
 4: { left: 14.40, top: 37.60, width: 22.00, height: 2.45, rotate: -3.40 },
 
-5: { left: 14.50, top: 43.40, width: 22.00, height: 2.45, rotate: -3.50 },
+5: { left: 14.50, top: 43.90, width: 22.00, height: 2.45, rotate: -3.10 },
 
   // PHASE 2
   6:  { left: 43.10, top: 19.40, width: 23.70, height: 2.45, rotate: -1 },

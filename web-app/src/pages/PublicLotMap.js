fde8e67dd27738c520a53a6f934fe2704005c394
altjohn143 <johnmatthewolano4@gmail.com>
2026-05-18 21:06:10 +0800

    esfd

--- web-app/src/pages/PublicLotMap.js
+++ web-app/src/pages/PublicLotMap.js
@@ -96,7 +96,7 @@ const LOT_POSITION_OVERRIDES = {
     mapTop: 64.06,
     mapWidth: 1.61,
     mapHeight: 2.94,
-    rotate: 27.49,
+    rotate: 0,
   },
   '2-1-3': {
     absolute: true,
@@ -104,7 +104,7 @@ const LOT_POSITION_OVERRIDES = {
     mapTop: 67.58,
     mapWidth: 1.61,
     mapHeight: 2.94,
-    rotate: 27.49,
+    rotate: 0,
   },
 };
 
